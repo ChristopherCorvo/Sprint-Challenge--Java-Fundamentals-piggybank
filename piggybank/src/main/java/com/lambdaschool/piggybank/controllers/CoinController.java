c76146a04d498accb091d2be20ed10ea5c5045be
Christopher Corvo <christophercorvo@gmail.com>
2020-12-04 16:33:03 -0500

    MVP

--- piggybank/src/main/java/com/lambdaschool/piggybank/controllers/CoinController.java
+++ piggybank/src/main/java/com/lambdaschool/piggybank/controllers/CoinController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLOutput;
@@ -63,7 +64,66 @@ public class CoinController
             total = total + (c.getQuantity() * c.getValue());
         }
         System.out.println("The piggy bank holds " + total);
-        return new ResponseEntity<>(total, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // Stretch:
+    // http://localhost:2019/money/{amount}
+
+    @GetMapping(value = "/money/{amount}", produces = "application/json")
+    public ResponseEntity<?> removeCoins(@PathVariable double amount)
+    {
+        List<Coin> myList = new ArrayList<>();
+        coinrepo.findAll().iterator().forEachRemaining(myList::add);
+
+        double total = 0.0;
+        double total2 = 0.0;
+        for(Coin c : myList)
+        {
+            total = total + (c.getQuantity() * c.getValue());
+        }
+
+        if(total < amount)
+        {
+            System.out.println("Not enough funds in piggy bank for this transaction.");
+        }
+        else
+        {
+            for(Coin c : myList)
+            {
+                if(c.getQuantity() >= 1)
+                {
+                    System.out.println("Line 96 " + amount);
+                    amount = amount - (c.getQuantity() * c.getValue());
+                    System.out.println("Line 97 " + c.getName() + (amount = amount - (c.getQuantity() * c.getValue())));
+                    System.out.println("Line 98 " + amount);
+                    c.setQuantity(0);
+                    System.out.println("Line 100 " + c.getQuantity());
+                }
+                else if(amount == 0.0)
+                {
+                    for(Coin c1 : myList)
+                    {
+                        if(c1.getQuantity() == 1)
+                        {
+                            System.out.println(c.getQuantity() + " " + c1.getName());
+                        }
+                        else
+                        {
+                            System.out.println(c1.getQuantity() + " " + c1.getNameplural());
+                        }
+
+                        for(Coin c2 : myList)
+                        {
+                            total2 = total2 + (c2.getQuantity() * c2.getValue());
+                            System.out.println(total2);
+                        }
+                    }
+                }
+            }
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
